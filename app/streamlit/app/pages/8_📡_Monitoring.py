c85bc0226890176ee818cb8f0fb8d4bf671dba3b
AntoinePELAMOURGUES <antoinepela@gmail.com>
2024-10-15 22:44:22 +0400

    MAJ GRAFANA

--- app/streamlit/app/pages/8_📡_Monitoring.py
+++ app/streamlit/app/pages/8_📡_Monitoring.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import streamlit.components.v1 as components
 
 if not st.session_state['is_logged_in']:
     st.warning("You need to be logged in to access this page.")
@@ -9,14 +8,8 @@ st.markdown("<h1 style='text-align: center;'>Testing & Monitoring</h1>", unsafe_
 
 grafana_url = "http://127.0.0.1:3000/d/ddz3bcm35rqiod/reco-movies-monitoring?orgId=1&from=1728991477901&to=1729013077901"
 
-# Dimensions personnalisées
-width = 1000  # Largeur en pixels
-height = 800  # Hauteur en pixels
-
-# Intégrer le tableau de bord Grafana avec des dimensions personnalisées
-components.html(
-    f'<iframe src="{grafana_url}" width="{width}" height="{height}" frameborder="0"></iframe>',
-    height=height,
-    width=width,
-    scrolling=True  # Permet le défilement si nécessaire
+# Embed the Grafana dashboard using an iframe
+st.markdown(
+    f'<iframe src="{grafana_url}" width="900" height="700" frameborder="0"></iframe>',
+    unsafe_allow_html=True
 )
